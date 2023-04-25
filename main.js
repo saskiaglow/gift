c02f50e7972a9ef3337a689b90e445be366f62c9
saskiaglow <saskia.globig@yale.edu>
2023-04-25 10:30:33 -0400

    touchscreen?

--- main.js
+++ main.js
@@ -22,6 +22,27 @@ image.onmouseup = function() {
 image.ondragstart = function() {
   return false;
 };
+image.ontouchstart = function(event) {
+  image.style.position = 'absolute';
+  image.style.zIndex = 1000;
+  document.body.append(image);
+  function moveAt(pageX, pageY) {
+    image.style.left = pageX - image.offsetWidth / 2 + 'px';
+    image.style.top = pageY - image.offsetHeight / 2 + 'px';
+  }
+  moveAt(event.pageX, event.pageY);
+  function ontouchmove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+  document.addEventListener('touchmove', ontouchmove);
+image.ontouchend = function() {
+    document.removeEventListener('touchmove', ontouchmove);
+    image.ontouchend = null;
+  };
+};
+image.ondragstart = function() {
+  return false;
+};
 function randint(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -32,6 +53,7 @@ function randomizeImg(el) {
 }
 let element = document.getElementById("image");
 randomizeImg(element);
+
 
 const image2 = document.getElementById('image2');
 image2.onmousedown = function(event) {
@@ -55,6 +77,27 @@ image2.onmouseup = function() {
 image2.ondragstart = function() {
   return false;
 };
+image2.ontouchstart = function(event) {
+  image2.style.position = 'absolute';
+  image2.style.zIndex = 1000;
+  document.body.append(image);
+  function moveAt(pageX, pageY) {
+    image2.style.left = pageX - image2.offsetWidth / 2 + 'px';
+    image2.style.top = pageY - image2.offsetHeight / 2 + 'px';
+  }
+  moveAt(event.pageX, event.pageY);
+  function ontouchmove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+  document.addEventListener('touchmove', ontouchmove);
+image2.ontouchend = function() {
+    document.removeEventListener('touchmove', ontouchmove);
+    image2.ontouchend = null;
+  };
+};
+image2.ondragstart = function() {
+  return false;
+};
 function randint(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -65,6 +108,7 @@ function randomizeImg(el) {
 }
 let element2 = document.getElementById("image2");
 randomizeImg(element2);
+
 
 const image3 = document.getElementById('image3');
 image3.onmousedown = function(event) {
@@ -88,6 +132,27 @@ image3.onmouseup = function() {
 image3.ondragstart = function() {
   return false;
 };
+image3.ontouchstart = function(event) {
+  image3.style.position = 'absolute';
+  image3.style.zIndex = 1000;
+  document.body.append(image);
+  function moveAt(pageX, pageY) {
+    image3.style.left = pageX - image3.offsetWidth / 2 + 'px';
+    image3.style.top = pageY - image3.offsetHeight / 2 + 'px';
+  }
+  moveAt(event.pageX, event.pageY);
+  function ontouchmove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+  document.addEventListener('touchmove', ontouchmove);
+image3.ontouchend = function() {
+    document.removeEventListener('touchmove', ontouchmove);
+    image3.ontouchend = null;
+  };
+};
+image3.ondragstart = function() {
+  return false;
+}
 function randint(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -122,6 +187,27 @@ image4.onmouseup = function() {
 image4.ondragstart = function() {
   return false;
 };
+image4.ontouchstart = function(event) {
+  image4.style.position = 'absolute';
+  image4.style.zIndex = 1000;
+  document.body.append(image);
+  function moveAt(pageX, pageY) {
+    image4.style.left = pageX - image4.offsetWidth / 2 + 'px';
+    image4.style.top = pageY - image4.offsetHeight / 2 + 'px';
+  }
+  moveAt(event.pageX, event.pageY);
+  function ontouchmove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+  document.addEventListener('touchmove', ontouchmove);
+image4.ontouchend = function() {
+    document.removeEventListener('touchmove', ontouchmove);
+    image4.ontouchend = null;
+  };
+};
+image4.ondragstart = function() {
+  return false;
+}
 function randint(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -156,6 +242,27 @@ image5.onmouseup = function() {
 image5.ondragstart = function() {
   return false;
 };
+image5.ontouchstart = function(event) {
+  image5.style.position = 'absolute';
+  image5.style.zIndex = 1000;
+  document.body.append(image);
+  function moveAt(pageX, pageY) {
+    image5.style.left = pageX - image5.offsetWidth / 2 + 'px';
+    image5.style.top = pageY - image5.offsetHeight / 2 + 'px';
+  }
+  moveAt(event.pageX, event.pageY);
+  function ontouchmove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+  document.addEventListener('touchmove', ontouchmove);
+image5.ontouchend = function() {
+    document.removeEventListener('touchmove', ontouchmove);
+    image5.ontouchend = null;
+  };
+};
+image5.ondragstart = function() {
+  return false;
+}
 function randint(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -166,6 +273,7 @@ function randomizeImg(el) {
 }
 let element5 = document.getElementById("image5");
 randomizeImg(element5);
+
 
 const image6 = document.getElementById('image6');
 image6.onmousedown = function(event) {
@@ -189,6 +297,27 @@ image6.onmouseup = function() {
 image6.ondragstart = function() {
   return false;
 };
+image6.ontouchstart = function(event) {
+  image6.style.position = 'absolute';
+  image6.style.zIndex = 1000;
+  document.body.append(image);
+  function moveAt(pageX, pageY) {
+    image6.style.left = pageX - image6.offsetWidth / 2 + 'px';
+    image6.style.top = pageY - image6.offsetHeight / 2 + 'px';
+  }
+  moveAt(event.pageX, event.pageY);
+  function ontouchmove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+  document.addEventListener('touchmove', ontouchmove);
+image6.ontouchend = function() {
+    document.removeEventListener('touchmove', ontouchmove);
+    image6.ontouchend = null;
+  };
+};
+image6.ondragstart = function() {
+  return false;
+}
 function randint(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -199,6 +328,7 @@ function randomizeImg(el) {
 }
 let element6 = document.getElementById("image6");
 randomizeImg(element6);
+
 
 const image7 = document.getElementById('image7');
 image7.onmousedown = function(event) {
@@ -222,6 +352,27 @@ image7.onmouseup = function() {
 image7.ondragstart = function() {
   return false;
 };
+image7.ontouchstart = function(event) {
+  image7.style.position = 'absolute';
+  image7.style.zIndex = 1000;
+  document.body.append(image);
+  function moveAt(pageX, pageY) {
+    image7.style.left = pageX - image7.offsetWidth / 2 + 'px';
+    image7.style.top = pageY - image7.offsetHeight / 2 + 'px';
+  }
+  moveAt(event.pageX, event.pageY);
+  function ontouchmove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+  document.addEventListener('touchmove', ontouchmove);
+image7.ontouchend = function() {
+    document.removeEventListener('touchmove', ontouchmove);
+    image7.ontouchend = null;
+  };
+};
+image7.ondragstart = function() {
+  return false;
+}
 function randint(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -232,6 +383,7 @@ function randomizeImg(el) {
 }
 let element7 = document.getElementById("image7");
 randomizeImg(element7);
+
 
 const image8 = document.getElementById('image8');
 image8.onmousedown = function(event) {
@@ -255,6 +407,27 @@ image8.onmouseup = function() {
 image8.ondragstart = function() {
   return false;
 };
+image8.ontouchstart = function(event) {
+  image8.style.position = 'absolute';
+  image8.style.zIndex = 1000;
+  document.body.append(image);
+  function moveAt(pageX, pageY) {
+    image8.style.left = pageX - image8.offsetWidth / 2 + 'px';
+    image8.style.top = pageY - image8.offsetHeight / 2 + 'px';
+  }
+  moveAt(event.pageX, event.pageY);
+  function ontouchmove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+  document.addEventListener('touchmove', ontouchmove);
+image8.ontouchend = function() {
+    document.removeEventListener('touchmove', ontouchmove);
+    image8.ontouchend = null;
+  };
+};
+image8.ondragstart = function() {
+  return false;
+}
 function randint(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -265,6 +438,7 @@ function randomizeImg(el) {
 }
 let element8 = document.getElementById("image8");
 randomizeImg(element8);
+
 
 const image9 = document.getElementById('image9');
 image9.onmousedown = function(event) {
@@ -288,6 +462,27 @@ image9.onmouseup = function() {
 image9.ondragstart = function() {
   return false;
 };
+image9.ontouchstart = function(event) {
+  image9.style.position = 'absolute';
+  image9.style.zIndex = 1000;
+  document.body.append(image);
+  function moveAt(pageX, pageY) {
+    image9.style.left = pageX - image9.offsetWidth / 2 + 'px';
+    image9.style.top = pageY - image9.offsetHeight / 2 + 'px';
+  }
+  moveAt(event.pageX, event.pageY);
+  function ontouchmove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+  document.addEventListener('touchmove', ontouchmove);
+image9.ontouchend = function() {
+    document.removeEventListener('touchmove', ontouchmove);
+    image9.ontouchend = null;
+  };
+};
+image9.ondragstart = function() {
+  return false;
+}
 function randint(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
